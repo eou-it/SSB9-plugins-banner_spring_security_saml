47cd2e1da4ed0e1dcb2977082d7287bf700dc293
mohit jain <mohit.jain@ellucian.com>
2015-09-09 15:34:55 +0530

    GAF-860 new Session Counter Listener test  cases added

--- test/integration/net.hedtech.banner/filter/SessionCounterListenerTest.groovy
+++ test/integration/net.hedtech.banner/filter/SessionCounterListenerTest.groovy
@@ -5,59 +5,64 @@
 package net.hedtech.banner.filter
 
 import net.hedtech.banner.security.SessionCounterListener
-import org.junit.Before
 import org.junit.FixMethodOrder
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runners.MethodSorters
-import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpSession
+import org.springframework.mock.web.MockServletContext
+import org.springframework.web.context.support.StaticWebApplicationContext
 
-import javax.servlet.http.HttpSession
 import javax.servlet.http.HttpSessionEvent
-import javax.servlet.http.HttpSessionListener
 
-import static org.junit.Assert.assertNotEquals
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class SessionCounterListenerTest {
 
-    def HttpSessionListener counterListener;
-    def HttpSession session;
-    def HttpSession session1;
-    def MockHttpServletRequest httpServletRequest;
-    def MockHttpServletRequest httpServletRequest1;
-    int totalActiveSession;
-    HttpSessionEvent event;
+    @Test
+    public void publishedEventIsReceivedbyListener() {
+        SessionCounterListener counterListener = new SessionCounterListener();
 
-    @Before
-    public void setup(){
-        counterListener= new SessionCounterListener();
-        httpServletRequest= new MockHttpServletRequest();
-        httpServletRequest1= new MockHttpServletRequest();
+        StaticWebApplicationContext context = new StaticWebApplicationContext();
+
+        MockServletContext servletContext = new MockServletContext();
+        servletContext.setAttribute(StaticWebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
+
+        context.setServletContext(servletContext);
+        context.registerSingleton("listener", SessionCounterListener.class, null);
+        context.refresh();
+
+        MockHttpSession session = new MockHttpSession(servletContext);
+        SessionCounterListener listener = (SessionCounterListener) context.getBean("listener");
+
+        HttpSessionEvent event = new HttpSessionEvent(session);
+
+        counterListener.sessionCreated(event);
+
+        assertNotNull(listener.getTotalActiveSession());
     }
 
     @Test
-    public void dummytest(){
-
+    public void sessionCreated() {
+        SessionCounterListener counterListener = new SessionCounterListener();
+        MockServletContext servletContext = new MockServletContext();
+        MockHttpSession session = new MockHttpSession(servletContext);
+        HttpSessionEvent event = new HttpSessionEvent(session);
+        int initialCount= counterListener.getTotalActiveSession();
+        counterListener.sessionCreated(event);
+        assertTrue("SessionCount",counterListener.getTotalActiveSession()>initialCount);
     }
 
-    @Ignore
-    public void "testSessionCreated"(){
-        session=httpServletRequest.getSession();
-        totalActiveSession=counterListener.getTotalActiveSession();
-        session1=httpServletRequest1.getSession();
-        int newSessionCount=counterListener.getTotalActiveSession();
-        assertNotEquals(newSessionCount, totalActiveSession);
+    @Test(expected=NullPointerException.class)
+    public void sessionDestroyed() {
+        SessionCounterListener counterListener = new SessionCounterListener();
+        MockServletContext servletContext = new MockServletContext();
+        MockHttpSession session = new MockHttpSession(servletContext);
+        HttpSessionEvent event = new HttpSessionEvent(session);
 
-
+        counterListener.sessionDestroyed(event);
     }
 
-    @Ignore
-    public void "testSessionDestroyed"(){
-        int activeSession= counterListener.getTotalActiveSession();
-        session= httpServletRequest.getSession(false);
-        session.invalidate();
-        assertTrue("Session Destroyed", counterListener.getTotalActiveSession() < activeSession);
-    }
+
 }
