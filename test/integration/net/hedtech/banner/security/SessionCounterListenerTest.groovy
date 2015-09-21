766fc5d7d5c1b0ff0872deab62f1db7ee17d384a
SphoortiA <sphoortiacharya@gmail.com>
2015-09-21 15:30:13 +0530

    GAF-860 Code review fixes

--- test/integration/net/hedtech/banner/security/SessionCounterListenerTest.groovy
+++ test/integration/net/hedtech/banner/security/SessionCounterListenerTest.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2009-2014 Ellucian Company L.P. and its affiliates.
+ Copyright 2015 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 
 package net.hedtech.banner.security
@@ -10,38 +10,16 @@ import org.junit.Test
 import org.junit.runners.MethodSorters
 import org.springframework.mock.web.MockHttpSession
 import org.springframework.mock.web.MockServletContext
-import org.springframework.web.context.support.StaticWebApplicationContext
+import org.springframework.web.context.WebApplicationContext
 
 import javax.servlet.http.HttpSessionEvent
 
-import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class SessionCounterListenerTest {
 
-    @Test
-    public void publishedEventIsReceivedbyListener() {
-        SessionCounterListener counterListener = new SessionCounterListener();
-
-        StaticWebApplicationContext context = new StaticWebApplicationContext();
-
-        MockServletContext servletContext = new MockServletContext();
-        servletContext.setAttribute(StaticWebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
-
-        context.setServletContext(servletContext);
-        context.registerSingleton("listener", SessionCounterListener.class, null);
-        context.refresh();
-
-        MockHttpSession session = new MockHttpSession(servletContext);
-        SessionCounterListener listener = (SessionCounterListener) context.getBean("listener");
-
-        HttpSessionEvent event = new HttpSessionEvent(session);
-
-        counterListener.sessionCreated(event);
-
-        assertNotNull(listener.getTotalActiveSession());
-    }
+    def grailsApplication
 
     @Test
     public void sessionCreated() {
@@ -54,14 +32,20 @@ class SessionCounterListenerTest {
         assertTrue("SessionCount",counterListener.getTotalActiveSession()>initialCount);
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test
     public void sessionDestroyed() {
         SessionCounterListener counterListener = new SessionCounterListener();
         MockServletContext servletContext = new MockServletContext();
+        servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, grailsApplication?.mainContext);
         MockHttpSession session = new MockHttpSession(servletContext);
         HttpSessionEvent event = new HttpSessionEvent(session);
 
+        int initialCount= counterListener.getTotalActiveSession();
+        counterListener.sessionCreated(event);
+        assertTrue("SessionCount",counterListener.getTotalActiveSession()>initialCount);
+
         counterListener.sessionDestroyed(event);
+        assertTrue("SessionCount",counterListener.getTotalActiveSession()==initialCount);
     }
 
 
