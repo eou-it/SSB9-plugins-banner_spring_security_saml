db1b0ddf527770e510dfddf692f51205196cc231
karthick <karthic.kumar@ellucian.com>
2015-12-02 20:06:08 +0530

    HRU-6302 - Updated version for release-platform-9.14.1

--- BannerSpringSecuritySamlGrailsPlugin.groovy
+++ BannerSpringSecuritySamlGrailsPlugin.groovy
@@ -13,7 +13,7 @@ import javax.servlet.Filter
 
 class BannerSpringSecuritySamlGrailsPlugin {
     // the plugin version
-    def version = "9.14"
+    def version = "9.14.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.5 > *"
 
