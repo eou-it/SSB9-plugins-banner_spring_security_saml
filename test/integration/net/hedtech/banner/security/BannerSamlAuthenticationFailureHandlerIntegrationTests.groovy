a07115b6b3ecf94f638c584a7a794f6140896fdc
Arun Ramachandra Upadhya <Arun.Upadhya@ellucian.com>
2017-04-10 14:58:53 +0530

    HRU-7173 added pending test cases to verify the failure messages in databases

--- test/integration/net/hedtech/banner/security/BannerSamlAuthenticationFailureHandlerIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/security/BannerSamlAuthenticationFailureHandlerIntegrationTests.groovy
@@ -1,16 +1,10 @@
-/** *****************************************************************************
- © 2017 SunGard Higher Education.  All Rights Reserved.
-
- CONFIDENTIAL BUSINESS INFORMATION
-
- THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
- AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
- NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
- WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
- ****************************************************************************** */
+/*******************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.security
 
 import grails.util.Holders
+import groovy.sql.Sql
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletRequest
 import org.codehaus.groovy.grails.plugins.testing.GrailsMockHttpServletResponse
@@ -18,23 +12,23 @@ import org.easymock.EasyMock
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.RedirectStrategy
 import org.springframework.security.web.WebAttributes
 
 /**
- * BannerSamlAuthenticationFailureHandlerIntegrationTests.
- *
- * Date: 3/28/2017
- * Time: 4:58 PM
+ * Integration test cases for BannerSamlAuthenticationFailureHandler.
  */
 class BannerSamlAuthenticationFailureHandlerIntegrationTests extends BaseIntegrationTestCase {
     BannerSamlAuthenticationFailureHandler bannerSamlAuthenticationFailureHandler
     GrailsMockHttpServletRequest request
     GrailsMockHttpServletResponse response
     AuthenticationException e
+    def authenticationDataSource
+
+    private static final String MSG = 'TEST_FAILURE_MESSAGE'
+    private static final String MODULE = 'TEST_FALURE_MODULE'
+    private static final String AUTH_NAME = 'TEST_FAILURE_AUTH_NAME'
 
     @Before
     public void setUp() {
@@ -50,6 +44,7 @@ class BannerSamlAuthenticationFailureHandlerIntegrationTests extends BaseIntegra
     @After
     public void tearDown() {
         super.tearDown()
+        deleteAllFailureHandlerDataFromDB()
     }
 
     @Test
@@ -84,5 +79,54 @@ class BannerSamlAuthenticationFailureHandlerIntegrationTests extends BaseIntegra
 
         // Request scope should be used for forward
         assertSame(request.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION), e)
+    }
+
+    @Test
+    public void testVerifyTheDataInDB() throws Exception {
+        bannerSamlAuthenticationFailureHandler.setDefaultFailureUrl("/target")
+
+        request.session.setAttribute('msg', MSG)
+        request.session.setAttribute('module', MODULE)
+        request.session.setAttribute('auth_name', AUTH_NAME)
+
+        bannerSamlAuthenticationFailureHandler.onAuthenticationFailure(request, response, e)
+        assert (request.getSession().getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION) == e)
+        assert (response.getRedirectedUrl() == "/target")
+
+        final String SELECT_QUERY = 'SELECT * FROM GURALOG ' +
+                'WHERE GURALOG_OBJECT = ? ' +
+                'AND GURALOG_USERID = ? ' +
+                'AND GURALOG_REASON = ? '
+
+        def sql
+        try {
+            def conn = authenticationDataSource.getConnection('BANSECR', 'u_pick_it')
+            sql = new Sql(conn)
+            sql.eachRow(SELECT_QUERY, [MODULE, AUTH_NAME, MSG], {
+                assertEquals(it.GURALOG_OBJECT, MODULE)
+                assertEquals(it.GURALOG_USERID, AUTH_NAME)
+                assertEquals(it.GURALOG_REASON, MSG)
+            })
+        } catch (e) {
+        } finally {
+            sql?.close()
+        }
+    }
+
+    private void deleteAllFailureHandlerDataFromDB() {
+        final String updateStatement = 'DELETE FROM GURALOG ' +
+                'WHERE GURALOG_OBJECT = ? ' +
+                'AND GURALOG_USERID = ? ' +
+                'AND GURALOG_REASON = ?'
+        def sql
+        try {
+            def conn = authenticationDataSource.getConnection('BANSECR', 'u_pick_it')
+            sql = new Sql(conn)
+            sql.executeUpdate(updateStatement, [MODULE, AUTH_NAME, MSG])
+            sql.commit()
+        } catch (e) {
+        } finally {
+            sql?.close()
+        }
     }
 }
