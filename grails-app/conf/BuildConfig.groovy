691ea344d665f9bed60fd512b5c6116b8de0bb73
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-02-09 11:11:59 +0530

    updating the copyright details HRU-7611

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 /*********************************************************************************
- Copyright 2015 Ellucian Company L.P. and its affiliates.
+ Copyright 2015-2018 Ellucian Company L.P. and its affiliates.
  **********************************************************************************/
 grails.servlet.version = "2.5"
 grails.project.class.dir = "target/classes"
