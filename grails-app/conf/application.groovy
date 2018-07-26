4f0e44cd06a6ab700e01f9b4a6a9134ac9c93592
Harshal Bramhankar <harshal.bramhankar@ellucian.com>
2018-07-26 15:21:09 +0530

    Changes for phase 2 banner speing security saml

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -3,7 +3,6 @@
  Copyright 2015-2018 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 //TODO Commenting the imports and corresponding changes as imports in not supported here
-//import net.hedtech.banner.configuration.ApplicationConfigurationUtils
 
 // ******************************************************************************
 //
@@ -13,11 +12,7 @@
 
 grails.config.locations = [] // leave this initialized to an empty list, and add your locations in the map below.
 //TODO Commenting the imports and corresponding changes as imports in not supported here
-/*def locationAdder = ApplicationConfigurationUtils.&addLocation.curry(grails.config.locations)
-println "App Name ${appName}"
-[ BANNER_APP_CONFIG:        "banner_configuration.groovy",
-  BANNER_SPRING_SECURITY_SAML_CONFIG: "banner_spring_security_saml_configuration.groovy",
-].each { envName, defaultFileName -> locationAdder( envName, defaultFileName ) }*/
+
 grails.config.locations.each {
        println "configuration: " + it
 }
