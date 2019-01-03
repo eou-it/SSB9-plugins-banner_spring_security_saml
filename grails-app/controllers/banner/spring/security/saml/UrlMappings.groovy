96804a3929b90af79740bb5b19fc0e19a6e2bc28
Ankur Jain <ankur.jain@ellucian.com>
2019-01-03 15:34:08 +0530

    Removing unnecessary default mapping - HRU-8280

--- grails-app/controllers/banner/spring/security/saml/UrlMappings.groovy
+++ grails-app/controllers/banner/spring/security/saml/UrlMappings.groovy
@@ -3,11 +3,6 @@ package banner.spring.security.saml
 class UrlMappings {
 
     static mappings = {
-        "/ssb/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/"(view:"/index")
         "500"(view:'/error')
